ee6d20fdc25f6c73097b5ab1225b6466bcd6a724
Stephen Smoogen <ssmoogen@redhat.com>
2023-02-01 13:53:56 +0000

    Update build_contaner to work across architectures
    The primary amount of work is done in the build_container.sh script
    which is used to determine architecture and then download the
    appropriate items from CentOS Stream koji for the container to work. A
    home directory and cross compiler environment volume are left for the
    next steps.
    
    Signed-off-by: Stephen Smoogen <ssmoogen@redhat.com>

--- Containerfile
+++ Containerfile
@@ -5,19 +5,20 @@ ENV NAME=autosd-cross-developer-toolbox
 MAINTAINER "Stephen Smoogen <ssmoogen@redhat.com>"
 
 LABEL com.github.containers.toolbox="true" \
-      com.gitlab.sgallagher.toolbox="true" \
-      com.redhat.component="$NAME" \
+      org.centos.component="$NAME" \
       name="$NAME" \
       version="$RELEASE" \
-      usage="This image is meant to be used with the toolbox command" \
+      usage="This image is meant to allow cross compiler using CentOS Stream 9"\
       summary="Container image for autosd cross development" \
       maintainer="$MAINTAINER"
 
 # Install the Red Hat certificate authority and the packages needed for
 # development on CentOS Stream and RHEL
-ADD  x86_pkgs /root/x86_pkgs
+
 COPY build_container.sh /root/
 RUN /root/build_container.sh
 
-VOLUME /opt/home
+# Use this to mount your home 
+VOLUME /home
+# Use this to mount an image built of cs9
 VOLUME /opt/cs9_aarch64
--- build_container.sh
+++ build_container.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-ARCH=$( arch )
+##
+## Flow of build script.
+## Determine Environment we are building in
+ARCH=$(arch)
+BUILD_DIR=/root/x86_pkgs
 
+## Setup core variables
 BASE_PKGS="
 bash
 binutils
@@ -29,18 +34,77 @@ xz
 xz-devel 
 zip 
 zlib-devel 
+vim-enhanced
+emacs-nox
 "
 
-# OK time to start working in the container and fixing things
+EPEL_PKGS="
+centpkg
+createrepo_c
+koji
+"
+
+## Define functions
+function x86_downloads(){
+    # Get needed gcc for current published CentOS Stream 9
+    GPKG=$( koji -p stream -q list-tagged --latest c9s-candidate gcc | awk '{print $1}' )
+    if [[ $? -ne 0 ]]; then
+	echo "Something went wrong with determining gcc. Exiting."
+	exit 4
+    else
+	echo "GCC is ${GPKG}"
+    fi
+    BPKG=$( koji -p stream -q list-tagged --latest c9s-candidate binutils | awk '{print $1}' )
+    if [[ $? -ne 0 ]]; then
+	echo "Something went wrong with determining binutils. Exiting."
+	exit 5
+    else
+	echo "binutils is ${GPKG}"
+    fi
+    
+    # download gcc from koji
+    koji -p stream download-build  --arch=x86_64 --arch=noarch ${GPKG}
+    if [[ $? -ne 0 ]]; then
+	echo "Something went wrong with download gcc. Exiting."
+	exit 6
+    fi
+    # download binutils from koji
+    koji -p stream download-build --arch=x86_64 --arch=noarch ${BPKG}
+    if [[ $? -ne 0 ]]; then
+	echo "Something went wrong with download gcc. Exiting."
+	exit 6
+    fi
+
+    # Set up a repository to make it easier to manage
+    /usr/bin/createrepo -v .
+}
+
+## Upgrade cs9 image to latest items
 dnf --assumeyes upgrade
+## Enable crb
 dnf --assumeyes install dnf-plugins-core 
 dnf --assumeyes config-manager --set-enabled crb 
+## Install EPEL-9
+dnf --assumeyes install epel-release
+## Install centpkg and koji from EPEL-9
 dnf --assumeyes install ${BASE_PKGS}
-# we only need to install the cross architecture stuff on x86_64
-if [[ ${ARCH} == "x86_64" ]]; then
-    cd /root/x86_pkgs/
-    dnf --assumeyes localinstall ./cross*aarch64*rpm
-else
-    echo ${ARCH}
+dnf --assumeyes install ${EPEL_PKGS}
+if [[ $? -ne 0 ]]; then
+    echo "Something went wrong with downloading epel packages. Exiting."
+    exit 1
 fi
+
+# OK time to start working in the container and fixing things
+if [[ ${ARCH} == "x86_64" ]]; then
+    ## Make cache directory
+    if [[ ! -d ${BUILD_DIR} ]]; then
+	mkdir -vp ${BUILD_DIR}
+    fi
+    pushd ${BUILD_DIR}
+    x86_downloads
+    dnf --assumeyes --nogpgcheck --repofrompath local,${BUILD_DIR} update
+    dnf --assumeyes --nogpgcheck --repofrompath local,${BUILD_DIR} install cross*aarch64*
+    popd
+fi
+
 dnf --assumeyes clean all
